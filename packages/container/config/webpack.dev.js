bc4be6e9417a492d67af8bbff7cbfa43143106a5
vishwanath-singh <vishwanath.singh@travclan.com>
2023-01-24 17:21:27 +0530

    created container deply script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const commonConfig = require('./webpack.common')
@@ -14,9 +14,7 @@ const devConfig = {
         }
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
+        
         new ModuleFederationPlugin({
             name:'container',
             remotes: {
